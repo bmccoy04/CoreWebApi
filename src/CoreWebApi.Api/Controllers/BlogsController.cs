2d34288e2c50ab655e32dd508580a08eb38e49f5
bmccoy04 <bmccoy@turner-industries.com>
2019-08-11 14:24:12 -0500

    feat: made update blog handler

--- src/CoreWebApi.Api/Controllers/BlogsController.cs
+++ src/CoreWebApi.Api/Controllers/BlogsController.cs
@@ -9,22 +9,22 @@ using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
 using CoreWebApi.Core.Entities;
 using CoreWebApi.Core.Interfaces;
+using CoreWebApi.Core.Dtos;
 
 namespace CoreWebApi.Api.Controllers
 {
     public class BlogsController : BaseApiV1Controller 
     {
         private readonly IMediator _mediator;
-        private readonly IMapper _mapper;
 
-        public BlogsController(IMediator mediator, IMapper mapper)
+        public BlogsController(IMediator mediator)
         {
             _mediator = mediator;
-            _mapper = mapper;
         }
 
         // GET: api/v1/Blogs
         [HttpGet]
+        [Produces(typeof(IEnumerable<BlogDto>))]
         public async Task<IActionResult> Get()
         {
             return Ok(await _mediator.Send(new GetBlogsQuery()));
@@ -39,16 +39,16 @@ namespace CoreWebApi.Api.Controllers
 
         // POST: api/Blogs
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] SaveBlogQuery blog)
+        public async Task<IActionResult> Post([FromBody] SaveBlogQuery blogQuery)
         {
-            return Ok(await _mediator.Send(blog));
+            return Ok(await _mediator.Send(blogQuery));
         }
 
         // PUT: api/Blogs/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put([FromBody] SaveBlogQuery blog)
+        public async Task<IActionResult> Put([FromBody] UpdateBlogQuery blogQuery)
         {
-            return Ok(await _mediator.Send(blog));
+            return Ok(await _mediator.Send(blogQuery));
         }
 
         // DELETE: api/ApiWithActions/5
--- src/CoreWebApi.Core/Handlers/UpdateBlogsQuery.cs
+++ src/CoreWebApi.Core/Handlers/UpdateBlogsQuery.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+
+using System.Threading;
 using System.Threading.Tasks;
 using CoreWebApi.Core.Entities;
 using CoreWebApi.Core.Interfaces;
@@ -7,28 +8,30 @@ using MediatR;
 
 namespace CoreWebApi.Core.Handlers
 {
-    public class SaveBlogQuery : IRequest<int>
+    public class UpdateBlogQuery : IRequest<int>
     {
+        public int Id { get; set; }
         public string Name { get; set; }
     }
-    public class SaveBlogQueryValidator : AbstractValidator<SaveBlogQuery>
+    public class UpdateBlogQueryValidator : AbstractValidator<UpdateBlogQuery>
     {
-        public SaveBlogQueryValidator() {
+        public UpdateBlogQueryValidator() {
+            RuleFor(x => x.Id).GreaterThan(0);
             RuleFor(x => x.Name).Length(0, 1);
         }
     }
 
 
-    public class SaveBlogHandler : IRequestHandler<SaveBlogQuery, int>
+    public class UpdateBlogHandler : IRequestHandler<UpdateBlogQuery, int>
     {
         private readonly IRepository _repository;
 
-        public SaveBlogHandler(IRepository repository)
+        public UpdateBlogHandler(IRepository repository)
         {
             _repository = repository;
         }
 
-        public Task<int> Handle(SaveBlogQuery request, CancellationToken cancellationToken)
+        public Task<int> Handle(UpdateBlogQuery request, CancellationToken cancellationToken)
         {
             var blog = new Blog() {Id  = 0, Name = request.Name};
 
